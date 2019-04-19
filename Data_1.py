7124e1d217cc2a3e64b174fbff1a91a22e195867
jorgitortiz <48936053+jorgitortiz@users.noreply.github.com>
2019-04-19 19:30:59 -0300

    Update Data_1.py

--- Data_1.py
+++ Data_1.py
@@ -2,7 +2,8 @@ import pandas as pd
 import numpy as np
 import matplotlib as plt
 from matplotlib import pyplot
-
+import seaborn as sns
+from scipy import stats
 
 def Course(df):
     df.replace("?", np.nan, inplace=True)
@@ -108,6 +109,136 @@ def Course(df):
     print(df.head())
 #   df.to_csv('clean_df.csv')
 
+### ANALYZING INDIVIDUAL FEATURE PATTERN USING VISUALIZATION
+    print("")
+    print(df.dtypes)
+    print(df['peak-rpm'].dtypes)
+    print("")
+# Calculate Correlation between variables float64 and int64
+    print(df[['bore', 'stroke', 'compression-ratio', 'horsepower']].corr())
+
+# Engine size as potential predictor variable of price
+    sns.set()
+    sns.regplot(x="engine-size", y="price", data=df)
+    print("")
+    #plt.ylim(0,0)
+# Examine correlation between engine-size and price
+    print(df[['engine-size','price']].corr())
+    print("")
+    sns.regplot(x="highway-mpg", y="price", data=df)
+    print(df[["highway-mpg","price"]].corr())
+    print("")
+    sns.regplot(x="peak-rpm", y="price", data=df)
+    print(df[["peak-rpm","price"]].corr())
+    print("")
+    sns.regplot(x="stroke", y="price", data=df)
+    print(df[["stroke","price"]].corr())
+# Categorical variable
+    sns.boxplot(x="body-style", y="price", data=df)
+    sns.boxplot(x="engine-location", y="price", data=df)
+    sns.boxplot(x="drive-wheels", y="price", data=df)
+    print("")
+### DESCRITIVE STATISTICAL ANALYSIS
+    print(df.describe(include=['object']))
+# How many object are in the variable
+    print(df['drive-wheels'].value_counts())
+# with the name of the variabel
+    print(df['drive-wheels'].value_counts().to_frame())
+# Repeat steps but with data frame called drive_wheels_counts
+    drive_wheels_counts = df['drive-wheels'].value_counts().to_frame()
+    drive_wheels_counts.rename(columns={'drive-wheels': 'value_counts'}, inplace=True)
+# Rename name index
+    drive_wheels_counts.index.name = 'drive-wheels'
+    print(drive_wheels_counts)
+    print("")
+    engine_loc_counts = df['engine-location'].value_counts().to_frame()
+    engine_loc_counts.rename(columns={'engine-location': 'value_counts'}, inplace=True)
+    engine_loc_counts.index.name = 'engine-location'
+    print(engine_loc_counts)
+    print("")
+
+### BASIC OF GROUPING
+
+    print(df['drive-wheels'].unique())
+    print("")
+    print(df['body-style'].unique())
+    print("")
+    df_group_one = df[['drive-wheels','body-style','price']]
+# Get mean with te variable object (drive-wheels)
+    df_group_one1 = df_group_one.groupby(['drive-wheels'], as_index=False).mean()
+    df_group_one2 = df_group_one.groupby(['body-style'], as_index=False).mean()
+    print(df_group_one1)
+    print("")
+    print(df_group_one2)
+    print("")
+# Get mean with two variable object
+    df_gptest = df[['drive-wheels', 'body-style', 'price']]
+    grouped_test1 = df_gptest.groupby(['drive-wheels', 'body-style'], as_index=False).mean()
+    print(grouped_test1)
+    print("")
+# With the excel
+    grouped_pivot = grouped_test1.pivot(index='drive-wheels', columns='body-style')
+    print(grouped_pivot)
+    print("")
+# Fill NaN with 0,but can anything value
+    grouped_pivot = grouped_pivot.fillna(0)  # fill missing values with 0
+    print(grouped_pivot)
+    print("")
+#use the grouped results
+    #plt.pyplot.figure()
+    #plt.pcolor(grouped_pivot, cmaps='RdBu')
+    #plt.colorbar()
+    #plt.show()
+#### grafica no se como se corre
+    pearson_coef, p_value = stats.pearsonr(df['wheel-base'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['horsepower'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P = ", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['length'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P = ", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['width'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['curb-weight'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['engine-size'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['bore'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['city-mpg'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+    pearson_coef, p_value = stats.pearsonr(df['highway-mpg'], df['price'])
+    print("The Pearson Correlation Coefficient is", pearson_coef, " with a P-value of P =", p_value)
+    print("")
+
+#### ANOVA
+#We can obtain the values of the method group using the method "get_group" with index
+    grouped_test2 = df_gptest[['drive-wheels', 'price']].groupby(['drive-wheels'])
+    print(grouped_test2.get_group('4wd')['price'])
+    print("")
+    f_val, p_val = stats.f_oneway(grouped_test2.get_group('fwd')['price'], grouped_test2.get_group('rwd')['price'],
+                                  grouped_test2.get_group('4wd')['price'])
+    print("ANOVA results: F=", f_val, ", P =", p_val)
+    print("")
+#Separately: fwd and rwd
+    f_val, p_val = stats.f_oneway(grouped_test2.get_group('fwd')['price'], grouped_test2.get_group('rwd')['price'])
+    print("ANOVA results: F=", f_val, ", P =", p_val)
+    print("")
+#Between 4wd and rwd
+    f_val, p_val = stats.f_oneway(grouped_test2.get_group('4wd')['price'], grouped_test2.get_group('rwd')['price'])
+    print("ANOVA results: F=", f_val, ", P =", p_val)
+    print("")
+#Between fwd and 4wd
+    f_val, p_val = stats.f_oneway(grouped_test2.get_group('fwd')['price'], grouped_test2.get_group('4wd')['price'])
+    print("ANOVA results: F=", f_val, ", P =", p_val)
+    print("")
 
 def main():
     other_path = "https://s3-api.us-geo.objectstorage.softlayer.net/cf-courses-data/CognitiveClass/DA0101EN/auto.csv"
